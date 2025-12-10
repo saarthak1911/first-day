e84f1dc8a3031fc070d14efac3b1abd68cba76b3
Sarthak Mane <sarthakmane475@gmail.com>
2025-12-10 22:55:08 +0530

    add sub

---
+++ arithmetic.py
@@ -0,0 +1,6 @@
+
+def add(num1 ,num2):
+    print("num1 + num2 = ", num1+num2)
+
+def subt(num1 , num2):
+    print("num1 - num2 = ", num1-num2)
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-
+import arithmetic
 print("Hello world")
+
+num1 =int(input("Enter 1st num: "))
+num2 = int(input("Enter 2nd num: "))
+
+arithmetic.add(num1, num2)
